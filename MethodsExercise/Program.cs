e62a342ec5718e413eb9b0d180e183d1e7204e73
Robert <broncofan3124@msn.com>
2024-11-18 11:59:21 -0800

    Completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, where are you from?");
+            string location = Console.ReadLine();
+            Console.WriteLine($"Does living in {location} affect your coding ability, {name}?");
+            string codingAbility = Console.ReadLine();
+            Console.WriteLine($"I see. Do you need any assistance?");
+            string assistance = Console.ReadLine();
+            Console.WriteLine($"Goodbye, {name}");
+
+        }
+
+        {
+            static Void MadLibInteraction();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, where are you from?");
+            string location = Console.ReadLine();
+            Console.WriteLine($"Does living in {location} affect your coding ability, {name}?");
+            string codingAbility = Console.ReadLine();
+            Console.WriteLine($"I see. Do you need any assistance?");
+            string assistance = Console.ReadLine();
+            Console.WriteLine($"Goodbye, {name}");
         }
     }
+    
 }
+    
+
